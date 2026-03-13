cb308c09edc72ea6cca0a0862d68fdec8058f01b
changdae <liedein@naver.com>
2026-03-13 22:01:01 +0900

    게시물 기본 마진 제거

--- html/index.php
+++ html/index.php
@@ -38,6 +38,25 @@ if (!in_array($page, $allowed_pages)) {
                     fontFamily: {
                         sans: ['Pretendard', 'sans-serif'],
                         serif: ['Merriweather', 'serif'],
+                    },
+                    typography: {
+                        DEFAULT: {
+                            css: {
+                                p: { margin: 0 },
+                                h1: { margin: 0 },
+                                h2: { margin: 0 },
+                                h3: { margin: 0 },
+                                h4: { margin: 0 },
+                                h5: { margin: 0 },
+                                h6: { margin: 0 },
+                                ul: { margin: 0 },
+                                ol: { margin: 0 },
+                                li: { margin: 0 },
+                                blockquote: { margin: 0 },
+                                pre: { margin: 0 },
+                                figure: { margin: 0 },
+                            }
+                        }
                     }
                 }
             }
